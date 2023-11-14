f6c07241f340bacdbbe0f2fb46c653b263fc7296
nayimjonrayimov <147370875+nayimjonrayimov@users.noreply.github.com>
2023-11-14 14:20:03 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -9,7 +9,7 @@ else if (consumed_water>30 && consumed_water<= 50)
 cost = 13 + 30 * 0.4 + (consumed_water-30) * 0.12;
 else if (consumed_water>50 && consumed_water<=60)
 cost = 13 + 30 * 0.4 + 20 * 0.12 + (consumed_water-50) *0.4;
-else 
+else if (consumed_water>60)
 cost = 13 + 30 * 0.4 + 20 * 0.12 + 10 * 1.4 + (consumed_water - 60) * 1.5;
 //cout << "The total cost is: " << cost << endl;
 
