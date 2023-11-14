c0f75542ff10bd8069137591a83ec3f5672e7a72
nayimjonrayimov <147370875+nayimjonrayimov@users.noreply.github.com>
2023-11-14 13:59:42 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,22 +1,25 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
-cout << "First number: " << endl;
+//cout << "First number: " << endl;
 cin >>x;
-cout << "Second number: " << endl;
-cin >>y;
-cout <<"Enter operation: ";
-cin >> operation;
+//cout << "Second number: " << endl;
+//cin >>y;
+//cout <<"Enter operation: ";
+//cin >> operation;
 if (operation=='+')
-cout << "sum: " << x+y << endl;
+   result=x+y;
+//cout << "sum: " << x+y << endl;
 else if (operation=='-')
-cout << "subtraction: " << x-y << endl;
+   result=x-y;
+//cout << "subtraction: " << x-y << endl;
 else if (operation=='*')
-cout << "multiplication: " << x*y << endl;
+   result=x*y;
+//cout << "multiplication: " << x*y << endl;
 else if (operation=='/')
-cout << "division: " << x/y << endl;
-else
-cout << "wrong operator\n";
+   result=x*y;
+//cout << "division: " << x/y << endl;
+
 
 
    return result;
