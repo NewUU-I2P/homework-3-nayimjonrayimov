b7e917afd606a071bc803fe90a21c7da537c0217
nayimjonrayimov <147370875+nayimjonrayimov@users.noreply.github.com>
2023-11-14 13:18:51 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -3,6 +3,20 @@
 std::string problemSolution3(float height, char S) {
     // write your code here
 
+cout << "Enter your sex (M or F) and height, correspondingly:\n ";
+cin >> S >> height;
+if ( (S == 'M' && height>=1.85) || (S == 'F' && height>=1.75))
+cout << "Tall\n";
+else if ( (S == 'M' && height>=1.70) || (S== 'F' && height>=1.60))
+cout << "Normal\n";
+else if ((S == 'M' && height<1.70) || (S == 'F' && height<1.60))
+cout << "Short\n";
+else
+cout << "Invalid output";
+
+    return 0;
+
+
     // use return to return your result
     // make use of control flow statements
 }
