1a0188456fc3a7c01544ab58e399f1e9b076bca6
nayimjonrayimov <147370875+nayimjonrayimov@users.noreply.github.com>
2023-11-14 14:24:07 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -2,10 +2,10 @@ float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
 
-cout << "Enter three numbers\n";
-cin >> i >> j >> k;
+//cout << "Enter three numbers\n";
+//cin >> i >> j >> k;
 result = (i>j && i>k) ? i : (j>i && j>k) ? j : k;
-cout << result << endl;
+//cout << result << endl;
  
 
 
