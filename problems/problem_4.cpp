0dccd0c8d5a3bc59c78b4361e54a5d5edce6c90a
nayimjonrayimov <147370875+nayimjonrayimov@users.noreply.github.com>
2023-11-14 14:31:52 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,6 +3,12 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
+    if (macAddress [1]%2==0) {
+        return "Unicast"; }
+    else if (macAddress [1] == 'F') {
+        return "Broadcast"; }
+    else { return "Multicast";}
+    
 
     // make use of control flow statements
     // return result;
